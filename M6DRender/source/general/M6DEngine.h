b7003ee8171e03746bda7b65f378d1ddddca3e74
Chillaxe <hellofellowdogz@gmail.com>
2025-12-07 19:23:12 +0000

    Update M6DEngine.h

--- M6DRender/source/general/M6DEngine.h
+++ M6DRender/source/general/M6DEngine.h
@@ -1,7 +1,30 @@
 #include <stdint.h>
+
+typedef struct m6dVector3_s {
+	float x, y, z;
+} m6dVector3;
+
+typedef struct m6dMatrix_s {
+	float m[4][4];
+} m6dMatrix;
+
+typedef struct m6dTransform_s {
+	m6dVector3 position;
+	m6dVector3 rotation;
+	m6dVector3 scale;
+	m6dMatrix* matrix;
+} m6dTransform;
+
+typedef struct m6dMotionPath_s {
+	// Length of motion path, in frames.
+	uint32_t length;
+	m6dTransform* frames;
+} m6dMotionPath;
+
 typedef struct m6dMesh_internal_s m6dMesh_internal;
 typedef struct m6dMesh_s {
 	m6dMesh_internal* internal;
+	m6dMotionPath* motionPath;
 } m6dMesh;
 
 typedef struct m6dRenderEngine_internal_s m6dRenderEngine_internal;
